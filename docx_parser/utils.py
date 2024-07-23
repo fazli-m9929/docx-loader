47781679e0cc8186b16933b13d9cdbcf79762c1f
fazli.m9929 <fazli.m9929@gmail.com>
2024-07-23 13:48:21 +0330

    TOC function added to extract and format TOC

--- docx_parser/utils.py
+++ docx_parser/utils.py
@@ -1,4 +1,5 @@
 from docx.oxml.text.paragraph import CT_P
+from lxml.etree import ElementTree, _Element
 
 def mathml_to_latex(element: CT_P):
     """ Convert MathML XML element to LaTeX string. """
@@ -63,3 +64,24 @@ def mathml_to_latex(element: CT_P):
 def contains_mathml(element: CT_P):
     xml_str = element.xml
     return '<m:' in xml_str
+
+
+def extract_toc_entries(xml_tree: _Element):
+    root = ElementTree(xml_tree).getroot()
+    namespaces = {
+        'w': 'http://schemas.openxmlformats.org/wordprocessingml/2006/main',
+    }
+
+    toc_entries = []
+
+    # Iterate over all paragraph elements
+    for para in root.findall('.//w:p', namespaces):
+        # Find the paragraph style
+        para_style = para.find('w:pPr/w:pStyle', namespaces)
+        if para_style is not None and para_style.get('{http://schemas.openxmlformats.org/wordprocessingml/2006/main}val', '').startswith('TOC'):
+            text_elements = para.findall('.//w:t', namespaces)
+            toc_text = '\t\tPage '.join([t.text or '' for t in text_elements if t.text])
+            if toc_text:  # Avoid adding empty entries
+                toc_entries.append(toc_text)
+
+    return '\n'.join(toc_entries)
